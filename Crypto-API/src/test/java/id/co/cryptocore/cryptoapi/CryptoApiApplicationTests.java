3e865edf6543d29ea68cba2a9cfb0ca1bb1e3ecb
Henkky <33210245+Henkky@users.noreply.github.com>
2023-05-04 08:17:10 +0700

    init crypto api from spring start

---
+++ Crypto-API/src/test/java/id/co/cryptocore/cryptoapi/CryptoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package id.co.cryptocore.cryptoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
